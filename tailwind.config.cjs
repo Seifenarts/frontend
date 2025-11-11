d83b414599065e9b9571c1c6e9f919df55a60bee
LutsDM <lutsdm@gmail.com>
2025-11-12 00:37:46 +0100

    feat(pageid): improve mobile layout and add MobileImageSlider component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: { 900: '900px' },
       maxWidth: { 'screen-xl': '1440px' },
       colors: {
         border: 'hsl(var(--border))',
